8e0440067be6765ab1325f62ac607bb1244ecc47
mrome007 <mrome007@ucr.edu>
2018-01-29 20:49:55 -0800

    Early setup for addition attack system

---
+++ Assets/Scripts/DartPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DartPlayer : Player
+{
+    [SerializeField]
+    private List<Additions> additions;
+}
+
+[Serializable]
+public class Additions
+{
+    public string Name;
+    public List<Addition> Addition;
+}
+
+[Serializable]
+public struct Addition
+{
+    [SerializeField]
+    private float damage;
+
+    public Addition(float dam)
+    {
+        damage = dam;
+    }
+
+    public float ApplyDamage(bool success)
+    {
+        damage = success ? damage : damage / 2f;
+        return damage;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class Player : MonoBehaviour 
+{
+    public event EventHandler ActionStart;
+    public event EventHandler ActionEnd;
+
+    protected virtual void StartAction()
+    {
+        var handler = ActionStart;
+        if(handler != null)
+        {
+            handler(this, null);
+        }
+    }
+
+    protected virtual void EndAction()
+    {
+        var handler = ActionEnd;
+        if(handler != null)
+        {
+            handler(this, null);
+        }
+    }
+}
