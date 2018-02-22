aefa5a3d9ef3a11b2cc1766f2f787d83dd1bfd37
mrome007 <mrome007@ucr.edu>
2018-02-21 21:24:31 -0800

    Implement jumping when in the overworld

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour 
+{
+    [SerializeField]
+    private Transform dartTransform;
+
+    private Vector3 movementVector;
+
+    private void Start()
+    {
+        movementVector.x = transform.position.x;
+        movementVector.y = transform.position.y;
+        movementVector.z = -10f;
+    }
+
+    private void Update()
+    {
+        movementVector.x = dartTransform.position.x;
+        transform.position = movementVector;
+    }
+}
--- Assets/Scripts/DartOverWorld.cs
+++ Assets/Scripts/DartOverWorld.cs
@@ -9,19 +9,53 @@ public class DartOverWorld : PlayerOverWorld
 
     [SerializeField]
     private DartOverWorldAnimation shadowAnimation;
-    
+
+    [SerializeField]
+    private Rigidbody2D dartRigidBody;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private bool grounded = false;
+
+    private Collider2D []results;
+
+    private Vector2 jumpForce;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        jumpForce = new Vector2(0f, 600f);
+        results = new Collider2D[1];
+    }
+
     private void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            dartRigidBody.AddForce(jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
     {
         DartMovement();
     }
 
     private void DartMovement()
     {
+        grounded = Physics2D.OverlapCircleNonAlloc(groundCheck.position, groundCheckRadius, results, (int)groundLayer) > 0;
+        
         var h = Input.GetAxis("Horizontal");
         dartOverWorldAnimation.DartWalk(h);
         shadowAnimation.DartWalk(h);
-        movementVector.x = h;
-        transform.Translate(movementSpeed * movementVector * Time.deltaTime);
+        movementVector.x = h * movementSpeed;
+        movementVector.y = dartRigidBody.velocity.y;
+
+        dartRigidBody.velocity = movementVector;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
