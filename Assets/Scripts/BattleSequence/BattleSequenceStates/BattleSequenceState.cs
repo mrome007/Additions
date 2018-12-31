0e36c2d177a72c3bf9df3ef2062f6710278ff979
mrome007 <mrome007@ucr.edu>
2018-12-30 18:28:28 -0800

    Create base class for battle sequence state. Will break up battle sequence script into several states instead of doing everything in one class.

---
+++ Assets/Scripts/BattleSequence/BattleSequenceStates/BattleSequenceState.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSequenceState : MonoBehaviour 
+{
+    public Action EnteredState;
+    public Action ExitedState;
+    
+    [SerializeField]
+    protected BattleSequenceState defaultNextState;
+
+    protected BattleSequenceState nextState;
+
+	public virtual void EnterState()
+	{
+        PostEnteredState();
+        
+        nextState = defaultNextState;
+	    RegisterEvents();	
+	}
+
+	public virtual void ExitState()
+	{
+	    UnRegisterEvents();
+        nextState.EnterState();
+
+        PostExitedState();
+	}
+
+	public virtual void RegisterEvents()
+	{
+	}
+
+	public virtual void UnRegisterEvents()
+    {
+	}
+
+    protected virtual void PostEnteredState()
+    {
+        EnteredState.Invoke();
+    }
+
+    protected virtual void PostExitedState()
+    {
+        ExitedState.Invoke();
+    }
+}
